d9e5f55cd4e60b24ae58390014f527cad8f1b27e
Nick Whitelegg <nickw4426@gmail.com>
2020-12-31 10:29:36 +0100

    add basic caching

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const fs = require('fs/promises');
 const db = require('./db');
 const TileModel = require('./models/TileModel');
 const tile = new TileModel(db);
@@ -10,13 +11,33 @@ app.get('/', (req, res) => {
 });
 
 app.get('/:z(\\d+)/:x(\\d+)/:y(\\d+).mvt', async (req, res) => {
+    const layers = req.query.layers.split(',');
+    const codes = tile.getCodes(layers);
+    let buffer, error = { };
+    const dir = `${process.env.CACHE_DIR}/${codes}/${req.params.z}/${req.params.x}`;
+    const filename = `${dir}/${req.params.y}.mvt`;
     try {
-        const buffer = await tile.getTiles(req.params, req.query.layers);
+        buffer = await fs.readFile(filename);
+    } catch(e) {
+        try {
+            buffer = await tile.getTiles(req.params, layers);
+        } catch(e) {
+            error = e;
+        }
+        if(buffer !== undefined) {
+            await fs.mkdir(dir, {
+                recursive: true,
+                mode: 0o755
+            });
+            fs.writeFile(filename, buffer);
+        }
+    }
+    if(buffer !== undefined) {
         res.setHeader('Content-Type', 'application/x-protobuf')
         res.send(buffer);
-    } catch(e) {
-        res.status(500).send(`ERROR ${e}`);
-        console.log(e);
+    } else {
+        console.log(error);
+        res.status(500).json(error);
     }
 });
 
--- server/models/TileModel.js
+++ server/models/TileModel.js
@@ -6,32 +6,36 @@ class TileModel {
      
             'lines': {
                 'cols' : 'highway, name, designation, ref, foot, horse, bicycle, access, waterway, railway, prow_ref, bridge, tunnel, power',
-                'table' : 'planet_osm_line'
+                'table' : 'planet_osm_line',
+                'code' : 'L'
             }, 
             'pois': {
                 'cols' : 'name, "natural", place, amenity, railway, power',
-                'table' : 'planet_osm_point'
+                'table' : 'planet_osm_point',
+                'code' : 'P'
             },
             'polygons': {
                 'cols' : 'name, "natural", landuse, military, building',
-                'table' : 'planet_osm_polygon'
+                'table' : 'planet_osm_polygon',
+                'code' : 'O'
             },
     
             'land' : {
                 'table' : 'land_polygons',
-                'geomcol' : 'geom'
+                'geomcol' : 'geom',
+                'code' : 'D'
             },
     
             'contours': {
                 'cols' : "CASE WHEN MOD(height, 50)=0 THEN 'major' ELSE 'minor' END AS contourtype, height",
-                'table' : 'contours'
+                'table' : 'contours',
+                'code' : 'C'
         }
     
 };
     }
 
     async getTiles({x, y, z}, layers) {
-        layers = layers.split(",");
         const buffers = [];
         for(let tilelayer of layers.filter(layer => this.layerData[layer] !== undefined)) {
             try {
@@ -56,6 +60,11 @@ class TileModel {
     async getTile(x, y, z, tilelayer, geomcol, cols, table) {
         const sql = `SELECT ST_AsMVT(row, '${tilelayer}', 4096, 'mvtgeom') FROM (SELECT envelope, ${cols}, ST_AsMVTGeom(${geomcol}, envelope, 4096, 256, true) AS mvtgeom FROM ${table}, (SELECT ST_TileEnvelope(${z},${x},${y}) AS envelope) AS e WHERE ${geomcol} && envelope) AS row`;
         return await this.db.query(sql);
+    }
+
+    getCodes(layers) {
+        return layers.filter(layer => this.layerData[layer] !== undefined)
+                    .map(layer => this.layerData[layer].code).join("");
     }
 }
 
